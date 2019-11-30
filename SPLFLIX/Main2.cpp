489c48a92359887aefb32a4766d79ced82f78b03
cohenteh <cohenteh@post.bgu.ac.il>
2019-11-30 22:13:24 +0200

    ss

---
+++ SPLFLIX/Main2.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include "../include/Session.h"
+using namespace std;
+
+int main(int argc, char** argv){
+	if(argc!=2)
+	{
+		cout << "usage splflix input_file" << endl;
+		return 0;
+	}
+	Session* s = new Session(argv[1]);
+	s->start();
+	Session s2 = *s;
+	delete(s);
+	s2.start();
+	return 0;
+}
--- SPLFLIX/main.cpp
+++ SPLFLIX/main.cpp
@@ -3,14 +3,12 @@
 using namespace std;
 
 int main(int argc, char** argv) {
-	/**if (argc != 2)
+	if (argc != 2)
 	{
 		cout << "usage splflix input_file" << endl;
 		return 0;
 	}
-	**/
-	Session s("config1.json");
-
+	Session s(argv[1]);
 	s.start();
 	return 0;
 }
